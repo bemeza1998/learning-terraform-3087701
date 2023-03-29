6f34c63f32945be5dd3088d9531a958acb8936b5
RyanDf <84430984+bemeza1998@users.noreply.github.com>
2023-03-29 13:35:09 -0500

    correción

--- main.tf
+++ main.tf
@@ -34,16 +34,16 @@ module "blog_vpc" {
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.9.0"
-  
-  name = "blog"
-  min_size = 1
-  max_size = 2
 
+  name = "blog"
+
+  min_size            = 1
+  max_size            = 2
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
-  security_groups = [module.blog_sg.security_group_id]
-  instance_type          = var.instance_type
-  image_id               = data.aws_ami.app_ami.id
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
@@ -82,7 +82,7 @@ module "blog_alb" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
+  version = "4.17.1"
 
   vpc_id  = module.blog_vpc.vpc_id
   name    = "blog"
